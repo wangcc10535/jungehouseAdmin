de6b9d6c5514964e0e0c05940382fa7b9ae615ba
wangcc <1053578651@qq.com>
2022-08-31 17:28:32 +0800

    项目初始化

---
+++ src/views/order/orderList.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Description: 
+ * @Author: wangcc
+ * @Date: 2022-08-31 17:23:30
+ * @LastEditors: wangcc
+ * @LastEditTime: 2022-08-31 17:23:31
+ * @FilePath: \jungehouseAdmin\src\views\order\orderList.vue
+ * @Copyright: Copyright (c) 2016~2022 by wangcc, All Rights Reserved. 
+-->
+<template>
+  <div>房产列表</div>
+</template>
+
+<script>
+export default {
+  name: 'orderList',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style  lang='scss' scoped>
+</style>
---
+++ src/views/rotation/rotation.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Description: 轮播图
+ * @Author: wangcc
+ * @Date: 2022-08-31 17:16:55
+ * @LastEditors: wangcc
+ * @LastEditTime: 2022-08-31 17:16:56
+ * @FilePath: \jungehouseAdmin\src\views\rotation\rotation.vue
+ * @Copyright: Copyright (c) 2016~2022 by wangcc, All Rights Reserved. 
+-->
+<template>
+<div>
+首页轮播图
+</div>
+</template>
+
+<script>
+export default {
+  name:'rotation',
+  data(){
+   return {
+
+   }
+  }
+}
+</script>
+
+<style  lang='scss' scoped>
+
+</style>
