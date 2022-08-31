291780387ac3010185cb4143646a23d22695fef2
wangcc <1053578651@qq.com>
2022-08-31 18:04:13 +0800

    常见问题

---
+++ src/views/question/question.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Description: 常见问题
+ * @Author: wangcc
+ * @Date: 2022-08-31 18:03:22
+ * @LastEditors: wangcc
+ * @LastEditTime: 2022-08-31 18:03:24
+ * @FilePath: \jungehouseAdmin\src\views\question\question.vue
+ * @Copyright: Copyright (c) 2016~2022 by wangcc, All Rights Reserved. 
+-->
+<template>
+<div>
+常见问题
+</div>
+</template>
+
+<script>
+export default {
+  name:'question',
+  data(){
+   return {
+
+   }
+  }
+}
+</script>
+
+<style  lang='scss' scoped>
+
+</style>
