943587f35b1d525d82f7ac2a1cbd29c0da5cfc0c
wangcc <1053578651@qq.com>
2022-09-02 17:34:57 +0800

    接口联调

--- vue.config.js
+++ vue.config.js
@@ -35,7 +35,8 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:9091`,
+        // target: `http://localhost:9091`,
+        target: `http://43.143.41.35:8080`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
