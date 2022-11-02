c37c3a7425f8a83d1a67b6e5e76f3e2ad259c559
wangcc <1053578651@qq.com>
2022-11-02 15:36:45 +0800

    公司视频修改

--- src/views/about/dialog/addVideo.vue
+++ src/views/about/dialog/addVideo.vue
@@ -13,13 +13,16 @@
                         :before-upload="beforeUploadVideo" :on-progress="uploadVideoProcess">
 
                         <!--视频区域，:src里面存放视频上传成功后的存储地址-->
-                        <video style="width: 100%;" v-if="showVideoPath !='' && !videoFlag"
+                        <!-- <video style="width: 100%;" v-if="showVideoPath != '' && !videoFlag"
                             :src="videoUpload.url2 + showVideoPath" class="avatar video-avatar" controls="controls">
                             您的浏览器不支持视频播放
-                        </video>
-
+                        </video> -->
+                        <video-player style="width: 50%;" v-if="showVideoPath != '' && !videoFlag"
+                            class="video-player vjs-custom-skin" ref="videoPlayer" :playsinline="true"
+                            :options="playerOptions">
+                        </video-player>
                         <!-- 变量showVideoPath如果不存在，就不显示，存在就显示视频 -->
-                        <i v-else-if="showVideoPath =='' && !videoFlag" class="el-icon-plus avatar-uploader-icon">
+                        <i v-else-if="showVideoPath == '' && !videoFlag" class="el-icon-plus avatar-uploader-icon">
                         </i>
                         <!--上传进度条区域-->
                         <el-progress :stroke-width="10" class="progressType" v-if="videoFlag == true" type="circle"
@@ -62,14 +65,36 @@ export default {
                 // 设置上传的请求头部
                 headers: { Authorization: "Bearer " + getToken() },
                 // 上传的地址
-                url: process.env.VUE_APP_BASE_API + "/common/upload",
-                url2: process.env.VUE_APP_BASE_API,
+                url: "http://49.247.22.30:8080/common/fileupload",
+                url2: "http://49.247.22.30:8080",
             },
             showVideoPath: "",
             uploadUrl: "",//你要上传视频到你后台的地址
             videoFlag: false, //是否显示进度条
             videoUploadPercent: "", //进度条的进度，
-            isShowUploadVideo: false //显示上传按钮
+            isShowUploadVideo: false, //显示上传按钮
+            playerOptions: {
+                playbackRates: [0.5, 1.0, 1.5, 2.0], // 可选的播放速度
+                autoplay: false,  // 如果为true,浏览器准备好时开始回放
+                muted: false,     // 默认情况下将会消除任何音频。
+                loop: false,      // 是否视频一结束就重新开始。
+                preload: 'auto',  // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+                language: 'zh-CN',
+                aspectRatio: '16:9',  // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+                fluid: true,  // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+                sources: [{
+                    type: "video/mp4",  // 类型
+                    src: ''             // url地址
+                }],
+                poster: '',  // 封面地址
+                notSupportedMessage: '此视频暂无法播放，请稍后再试',  // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
+                controlBar: {
+                    timeDivider: true,           // 当前时间和持续时间的分隔符
+                    durationDisplay: true,       // 显示持续时间
+                    remainingTimeDisplay: false, // 是否显示剩余时间功能
+                    fullscreenToggle: true       // 是否显示全屏按钮
+                }
+            },
         };
     },
     methods: {
@@ -107,6 +132,7 @@ export default {
             if (res != "") {
                 this.showVideoPath = res.url;
                 this.addorputForm.dictValue = res.url;
+                this.playerOptions.sources[0].src = res.url;
                 this.form.courseUrl = res.url;
             } else {
                 this.$message.error('视频上传失败，请重新上传！');
@@ -152,8 +178,9 @@ export default {
                 // console.log(obj);
                 this.addorputForm = obj;
                 this.showVideoPath = obj.dictValue;
+                this.playerOptions.sources[0].src = obj.dictValue;
                 this.isShowUploadVideo = true //显示上传按钮
-                
+
             }
         },
         fileList(img) {
